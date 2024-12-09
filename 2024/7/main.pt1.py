8337114fd2ef889e5ac65fe4959d00daa425d108
Jack Basha <jack.basha@yahoo.com>
2024-12-09 15:21:41 -0800

    Solved 2024/7

---
+++ 2024/7/main.pt1.py
@@ -0,0 +1,28 @@
+
+def find_ops(res, numbers, i, acc):
+    if i == len(numbers):
+        return acc == res
+    
+    if i == 0:
+        return find_ops(res, numbers, i + 1, numbers[i])
+    else:
+        return find_ops(res, numbers, i + 1, acc + numbers[i]) or \
+                find_ops(res, numbers, i + 1, acc * numbers[i])
+
+def main():
+    f = open("./input.txt", "r")
+
+    ans = 0
+    for line in f:
+        l = line.split(":")
+        res = int(l[0])
+        numbers = l[1].strip().split()
+        numbers = list(map(lambda x: int(x), numbers))
+
+        if find_ops(res, numbers, 0, 0):
+            ans += res
+
+    print(ans)
+
+if __name__ == "__main__":
+    main()
