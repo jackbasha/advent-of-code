7d33ee8550492a6321fa19de00cebc71a26da37c
Jack Basha <jack.basha@yahoo.com>
2024-12-10 09:17:11 -0800

    Finished 2024/8.1 and 8.2

--- 2024/8/main.pt1.py
+++ 2024/8/main.pt1.py
@@ -1,7 +1,7 @@
 
 
 def main():
-    f = open("./small_input.txt", "r")
+    f = open("./input.txt", "r")
 
     ans = 0
 
@@ -15,7 +15,12 @@ def main():
     for line in f:
         j = 0
         for c in line:
-            if c != '.' and c != '\n':
+            if i == 0:
+                print(c, j)
+            if c == '\n':
+                continue
+
+            if c != '.':
                 if c in symbol_locations:
                     symbol_locations[c].append((i, j))
                 else:
@@ -31,7 +36,31 @@ def main():
 
     antinodes = [[False for j in range(m)] for i in range(n)]
 
-    print(antinodes, len(antinodes), len(antinodes[0]))
+    for c in symbol_locations:
+        for i in range(len(symbol_locations[c])):
+            for j in range(i + 1, len(symbol_locations[c])):
+                a = symbol_locations[c][i]
+                b = symbol_locations[c][j]
+
+                d = (a[0] - b[0], a[1] - b[1])
+
+                anti1 = (a[0] + d[0], a[1] + d[1])
+                anti2 = (b[0] - d[0], b[1] - d[1])
+
+                if anti1[0] >= 0 and anti1[0] < n and anti1[1] >= 0 and anti1[1] < m:
+                    antinodes[anti1[0]][anti1[1]] = True
+
+                if anti2[0] >= 0 and anti2[0] < n and anti2[1] >= 0 and anti2[1] < m:
+                    antinodes[anti2[0]][anti2[1]] = True
+
+    for i in range(n):
+        for j in range(m):
+            if antinodes[i][j]:
+                print("#", end = "")
+                ans += 1
+            else:
+                print(".", end = "")
+        print()
 
     print(ans)
 
--- 2024/8/main.pt2.py
+++ 2024/8/main.pt2.py
@@ -1,6 +1,90 @@
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __sub__(self, other):
+        return Point(self.x - other.x, self.y - other.y)
+    
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+
+    def mul(self, k):
+        return Point(self.x * k, self.y * k)
+
 def main():
     f = open("./input.txt", "r")
 
+    ans = 0
+
+    symbol_locations = {}
+    n = None
+    m = None
+
+    i = 0
+    j = 0
+
+    for line in f:
+        j = 0
+        for c in line:
+            if i == 0:
+                print(c, j)
+            if c == '\n':
+                continue
+
+            if c != '.':
+                if c in symbol_locations:
+                    symbol_locations[c].append(Point(i, j))
+                else:
+                    symbol_locations[c] = [Point(i, j)]
+            j += 1
+
+        if m == None:
+            m = j
+        i += 1
+
+    if n == None:
+        n = i
+
+    antinodes = [[False for j in range(m)] for i in range(n)]
+
+    for c in symbol_locations:
+        for i in range(len(symbol_locations[c])):
+            for j in range(i + 1, len(symbol_locations[c])):
+                a = symbol_locations[c][i]
+                b = symbol_locations[c][j]
+
+                antinodes[a.x][a.y] = True
+                antinodes[b.x][b.y] = True
+
+                d = a - b
+
+                k = 1
+                a_prime = a + d.mul(k)
+
+                while(a_prime.x >= 0 and a_prime.x < n and a_prime.y >= 0 and a_prime.y < m):
+                    antinodes[a_prime.x][a_prime.y] = True
+                    k += 1
+                    a_prime = a + d.mul(k)
+
+                k = 1
+                b_prime = b - d.mul(k)
+                while(b_prime.x >= 0 and b_prime.x < n and b_prime.y >= 0 and b_prime.y < m):
+                    antinodes[b_prime.x][b_prime.y] = True
+                    k += 1
+                    b_prime = b - d.mul(k)
+
+    for i in range(n):
+        for j in range(m):
+            if antinodes[i][j]:
+                print("#", end = "")
+                ans += 1
+            else:
+                print(".", end = "")
+        print()
+
+    print(ans)
 
 if __name__ == "__main__":
     main()
