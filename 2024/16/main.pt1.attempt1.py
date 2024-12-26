This still produces wrong results, going back to basics

2024/16/main.pt1.attempt1.py
@@ -9,6 +9,23 @@ class DIRECTIONS(Enum):
     SOUTH = 2
     WEST = 3
 
+class MemoInstance:
+    def __init__(self, val=None, dir=None):
+        self.val = np.inf if val == None else val
+        self.dir = 'Z' if dir == None else dir
+    
+    def __eq__(self, other):
+        return self.val == other.val
+
+    def __le__(self, other):
+        return self.val <= other.val
+
+    def __gt__(self, other):
+        return self.val > other.val
+
+    def __repr__(self):
+        return str(self.val) + ", " + self.dir
+
 def tranlate_direction(dir):
     if (dir == 'S'):
         return DIRECTIONS.SOUTH.value
@@ -40,15 +57,18 @@ class Solver:
         self.grid = grid
         # self.DIRECTIONS = ['S', 'N', 'E', 'W']
         self.best_known_cost = np.inf
-        self.visited = [[([False] * 4) for j in range(len(grid[0]))] for i in range(len(grid))]
-        self.memo = [[([np.inf] * 4) for j in range(len(grid[0]))] for i in range(len(grid))]
-        self.cost = [[([np.inf] * 4) for j in range(len(grid[0]))] for i in range(len(grid))]
+        self.visited = [[False for j in range(len(grid[0]))] for i in range(len(grid))]
+        self.memo = [[MemoInstance() for j in range(len(grid[0]))] for i in range(len(grid))]
+        self.cost = [[MemoInstance() for j in range(len(grid[0]))] for i in range(len(grid))]
 
     def solve(self, i, j, dir, cost):
+        print(i, j)
+
         if (self.grid[i][j] == 'E'):
             # Use for early terminating
             self.best_known_cost = min(self.best_known_cost, cost)
-            return 0
+            self.cost[i][j] = min(self.cost[i][j], MemoInstance(cost, 'Z'))
+            return MemoInstance(cost, dir)
 
         # if (cost >= self.best_known_cost):
         #     return np.inf
@@ -56,35 +76,44 @@ class Solver:
         ts = tranlate_direction(dir)
         print(ts, dir)
         
-        if (self.visited[i][j][ts]):
-            if (cost > self.cost[i][j][ts]):
-                return self.memo[i][j][ts]
+        if (self.visited[i][j]):
+            ret = None
+
+            if (dir != self.memo[i][j].dir):
+                ret = MemoInstance(self.memo[i][j].val + 1000, self.memo[i][j].dir)
+            else:
+                ret = MemoInstance(self.memo[i][j].val, self.memo[i][j].dir)
+            
+            self.cost[i][j] = min(self.cost[i][j], MemoInstance(cost, dir))
+            return ret
         
-        self.visited[i][j][ts] = True
-        self.cost[i][j][ts] = cost
+        self.visited[i][j] = True
+        self.cost[i][j].val = cost
+        self.cost[i][j].dir = dir
 
         if (self.grid[i][j] == '#'):
-            return np.inf
+            return MemoInstance(np.inf, None)
 
         ts_coords = translate_direction_to_coords(dir)
-        self.memo[i][j][ts] = min(
-            self.memo[i][j][ts],
-            self.solve(i + ts_coords[0], j + ts_coords[1], dir, cost + 1) + 1
-        )
+        try:
+            self.memo[i][j] = min(
+                self.memo[i][j],
+                self.solve(i + ts_coords[0], j + ts_coords[1], dir, cost + 1)
+            )
+        except TypeError:
+            print(self.memo[i][j], self.solve(i + ts_coords[0], j + ts_coords[1], dir, cost + 1))
+            exit
 
         for d in rotation_options(dir):
-            if (i == 5 and j == 1):
-                print("HERE" + d)
-
             ts = tranlate_direction(d)
             ts_coords = translate_direction_to_coords(d)
 
-            self.memo[i][j][ts] = min(
-                self.memo[i][j][ts],
-                self.solve(i + ts_coords[0], j + ts_coords[1], d, cost + 1001) + 1001
+            self.memo[i][j] = min(
+                self.memo[i][j],
+                self.solve(i + ts_coords[0], j + ts_coords[1], d, cost + 1001)
             )
 
-        return self.memo[i][j][ts]
+        return self.memo[i][j]
 
 def main():
     f = open("/home/jack/projects/advent-of-code/2024/16/small_input2.txt", "r")