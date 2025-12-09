4424c53c607ea7da743dc349c3bf1ba718759bab
Jack Basha <jack.basha@yahoo.com>
2025-12-08 23:06:49 -0800

    Edited the script yet again to also fetch sample data and add that in a nicely-structed format. Also changed the temp file's name to init_main to indicate its function. Added some initial code in there to test against the sample inputs and report any discrepency. Finally, added an interactive puzzle file so I can look into the structure of the Puzzle object

--- create_new_puzzle_folder_y_d_input.py
+++ create_new_puzzle_folder_y_d_input.py
@@ -6,7 +6,7 @@ https://github.com/wimglenn/advent-of-code-wim/issues/1
 
 """
 
-from aocd import get_data
+from aocd.models import Puzzle
 import shutil
 import pathlib
 import sys
@@ -15,19 +15,34 @@ import os
 
 def main():
     y, d = map(lambda x: int(x), sys.argv[1:])
-    curr_dir = pathlib.Path(__file__).parent.resolve()
+    p = Puzzle(year=y, day=d)
 
+    curr_dir = pathlib.Path(__file__).parent.resolve()
     dir_to_make = curr_dir / "{}/{}".format(y, d)
 
     if dir_to_make.is_dir():
-        print("Folder already exists")
-        sys.exit(1)
+        print("Folder already exists, refetching input only...")
+    else:
+        os.makedirs(dir_to_make)
+        shutil.copy(curr_dir / "_temp_file.py", dir_to_make / "main.py")
 
-    os.makedirs(dir_to_make)
-    shutil.copy(curr_dir / "_temp_file.py", dir_to_make / "main.py")
-
+    # Write input data to input.txt file
     f = open(dir_to_make / "input.txt", "w")
-    f.write(get_data(day=d, year=y))
+    f.write(p.input_data)
+
+    for i in range(len(p.examples)):
+        example_file_name = "example_input_{}.txt".format(str(i))
+        example_file = open(dir_to_make / example_file_name, "w")
+        example_file.write(p.examples[i].input_data)
+
+        answer_a_file_name = "answer_a_{}.txt".format(str(i))
+        answer_a_file = open(dir_to_make / answer_a_file_name, "w")
+        answer_a_file.write(p.examples[i].answer_a)
+
+        answer_b_file_name = "answer_b_{}.txt".format(str(i))
+        answer_b_file = open(dir_to_make / answer_b_file_name, "w")
+        answer_b_file.write(p.examples[i].answer_b)
+
 
 if __name__ == "__main__":
     main()
